f79381437cb397e96501d5a1b075e74dfaef3aa9
skarlalu <145408766+skarlalu@users.noreply.github.com>
2026-06-21 21:15:28 -0500

    Delete producto.py

--- producto.py
+++
@@ -1,7 +0,0 @@
-class Producto:
-    def __init__(self, nombre, precio):
-        self.nombre = nombre
-        self.precio = precio
-
-    def __str__(self):
-        return f"{self.nombre} - ${self.precio:.2f}"
