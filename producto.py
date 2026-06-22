a06bfe709edd5d0204a079313e40b5e21a4efac2
skarlalu <145408766+skarlalu@users.noreply.github.com>
2026-06-21 21:10:52 -0500

    Add files via upload

---
+++ producto.py
@@ -0,0 +1,7 @@
+class Producto:
+    def __init__(self, nombre, precio):
+        self.nombre = nombre
+        self.precio = precio
+
+    def __str__(self):
+        return f"{self.nombre} - ${self.precio:.2f}"
