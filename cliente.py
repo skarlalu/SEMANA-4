6a2509040ffec57802517222aff9f7acf32c3b4c
skarlalu <145408766+skarlalu@users.noreply.github.com>
2026-06-21 21:14:45 -0500

    Delete cliente.py

--- cliente.py
+++
@@ -1,6 +0,0 @@
-class Cliente:
-    def __init__(self, nombre, mesa):
-        self.nombre = nombre
-        self.mesa = mesa
-    def __str__(self):
-        return f"Cliente: {self.nombre} (Mesa {self.mesa})"
